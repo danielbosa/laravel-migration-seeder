a164287449cc94fb6c0a18e1a8cb02345d3d37af
danielbosa <danielbosa95@gmail.com>
2024-05-23 08:38:06 +0200

    Migration prepared

--- database/migrations/2024_05_23_062844_create_treni_trenord_table.php
+++ database/migrations/2024_05_23_062844_create_treni_trenord_table.php
@@ -15,6 +15,19 @@ return new class extends Migration
     {
         Schema::create('treni_trenord', function (Blueprint $table) {
             $table->id();
+            $table->string('company', 255);
+            $table->string('departure', 255);
+            $table->string('destination', 255);
+            $table->date('departure_date'); 
+            $table->time('departure_time');
+            $table->date('departure_date'); 
+            $table->time('departure_time');
+            $table->date('arrival_date');
+            $table->date('arrival_time');
+            $table->string('train_id',20);
+            $table->integer('carriage_count');
+            $table->boolean('on_time');
+            $table->boolean('confirmed');
             $table->timestamps();
         });
     }
